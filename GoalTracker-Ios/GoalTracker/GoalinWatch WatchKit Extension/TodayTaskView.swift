c88fb2514eee4831fd1b3b7b1b06c0345952c53c
Mikhael Marpaung <mikhaelbm@gmail.com>
2022-07-15 07:06:04 +0700

    0.2

--- GoalTracker-Ios/GoalTracker/GoalinWatch WatchKit Extension/TodayTaskView.swift
+++ GoalTracker-Ios/GoalTracker/GoalinWatch WatchKit Extension/TodayTaskView.swift
@@ -9,40 +9,52 @@ import SwiftUI
 
 struct TodayTaskView: View {
     
-    @State var isTapped: Bool = false
-    
+    private struct Goals: Identifiable {
+        let goalColor: Color
+//        let isCompleted: Bool
+        let activityName: String
+        let subtaskName: String
+        let subtaskDate: String
+        var id: String {activityName}
+    }
+    private let todayTasks: [Goals] = [
+        Goals(goalColor: .red, activityName: "Activity 1", subtaskName: "Subtask 1", subtaskDate: "15.00"),
+        Goals(goalColor: .teal, activityName: "Activity 2", subtaskName: "Subtask 2", subtaskDate: "16.00"),
+        Goals(goalColor: .purple, activityName: "Activity 3", subtaskName: "Subtask 3", subtaskDate: "15.00")
+    ]
+    @State private var isCompleted: Bool = false
     var body: some View {
         ScrollView{
             VStack(alignment: .leading, spacing: 3) {
-                ForEach((1...3), id: \.self) {
-                    Text("Activity \($0)")
+                ForEach(todayTasks) {Goals in
+                    Text(Goals.activityName)
                         .font(.title3)
                         .fontWeight(.semibold)
                     
                     Button{
-                        isTapped.toggle()
+                        isCompleted.toggle()
                     } label: {
                         Image(systemName: "rectangle.fill")
                             .resizable()
                             .frame(width: 15, height: .infinity)
                             .padding(.leading, -15)
-                            .foregroundColor(isTapped ? .gray.opacity(0.8) : .white)
+                            .foregroundColor(isCompleted ? .gray.opacity(0.8) : Goals.goalColor)
                         
                         VStack(alignment: .leading, spacing: 3){
                             HStack{
-                                Image(systemName: isTapped ? "checkmark.circle" : "circle")
+                                Image(systemName: isCompleted ? "checkmark.circle" : "circle")
                                     .resizable()
                                     .frame(width: 20, height: 20)
                                     .foregroundColor(.gray)
                                 
-                                Text("Subtask 1")
+                                Text(Goals.subtaskName)
                                     .font(.title3)
                                     .fontWeight(.semibold)
-                                    .strikethrough(isTapped)
-                                    .foregroundColor(isTapped ? .gray.opacity(0.8) : .white)
+                                    .strikethrough(isCompleted)
+                                    .foregroundColor(isCompleted ? .gray.opacity(0.8) : .white)
                             }
                             
-                            Text("17.50")
+                            Text(Goals.subtaskDate)
                                 .font(.caption)
                                 .padding(.leading, 25)
                                 .foregroundColor(.gray)
